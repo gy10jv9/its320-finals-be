2aca87a3e324783a66bda55199facbc3ed41b326
vincentm.work.2023@gmail.com <vincentm.work.2023@gmail.com>
2024-05-18 14:35:16 +0800

    reverted

--- lib/prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = (globalThis as any).prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  (globalThis as any).prisma = prisma;
-}
-
-export default prisma;
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,19 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => { 
+//     res.header("Access-Control-Allow-Origin",  
+//                "http://localhost:4200"); 
+//     res.header("Access-Control-Allow-Headers",  
+//                "Origin, X-Requested-With, Content-Type, Accept"); 
+//     next(); 
+// });
+// app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  	res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
