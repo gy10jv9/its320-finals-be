21159a3352b81612d3bb020b8a16ce44c8ce3c08
Vincent Manalo <146041946+gy10jv9@users.noreply.github.com>
2024-06-01 07:10:00 +0000

    added cors

--- index.ts
+++ index.ts
@@ -1,24 +1,30 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import userRouter from "./routes/user";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => { 
-//     res.header("Access-Control-Allow-Origin",  
-//                "http://localhost:4200"); 
-//     res.header("Access-Control-Allow-Headers",  
-//                "Origin, X-Requested-With, Content-Type, Accept"); 
-//     next(); 
-// });
-// app.use(express.json());
+// -----[ PARA SA CORS ]-----
+app.use(cors({
+    origin: 'http://localhost:4200',
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+}));
 
+app.use(express.json());
+
+// -----[ PARA SA ROOT / PARA MABAL AN NGA GAGANA ]-----
 app.get("/", (req: Request, res: Response) => {
-  	res.send("Gagana na sir!");
+  	res.send("Hello World!");
 });
 
+// -----[ PARA SA USER ]-----
+app.use("/user", userRouter);
+
+// -----[ PARA SA CONSOLE LOG ]-----
 app.listen(port, () => {
   	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
