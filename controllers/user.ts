3814371387f5e4d029f409ba3ebed30172977872
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-06-05 21:48:43 +0800

    add user function

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,1 +1,13 @@
-import { Express } from "express";
+import prisma from "../library/prisma";
+
+export const adduser = async (payload: any) => {
+    let query = await prisma.user.create({
+        data: {
+            username: payload.username,
+            email: payload.email,
+            password: payload.password
+        }
+    })
+    
+    return query
+}
--- routes/user.ts
+++ routes/user.ts
@@ -1,9 +1,17 @@
 import { Router } from "express"
+import { adduser } from "../controllers/user"
 
 const userRouter = Router()
 
 userRouter.get("/", (req, res) => {
     res.send("User route is working!")
+})
+userRouter.post("/add", async (req, res) => {
+    let payload = req.body
+    let addedUser = await adduser(payload)
+
+    console.log(`[server]: Successfully added ${JSON.stringify(addedUser)}`)
+    return res.json(addedUser)
 })
 
 export default userRouter
