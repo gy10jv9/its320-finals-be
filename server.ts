35104ae12f5efccafb42f051ed713e71525ec54a
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-06-12 09:04:31 +0800

    refactor: Add support for additional HTTP methods in CORS headers

--- server.ts
+++ server.ts
@@ -13,7 +13,8 @@ const port = process.env.PORT || 3000;
 // -----[ PARA SA CORS ]-----
 app.use(cors({
     origin: ['http://localhost:4200', 'https://its320-finals-be.onrender.com'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
 app.use(express.json());
